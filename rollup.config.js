8f9c237c9e764b317bf3667f106ef6017973ac25
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-16 14:31:55 +0000

    Iteration

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ const execSync = require('child_process').execSync;
 
 const babel = require('rollup-plugin-babel');
 const commonjs = require('rollup-plugin-commonjs');
-const compiler = require('@ampproject/rollup-plugin-closure-compiler');
+//const compiler = require('@ampproject/rollup-plugin-closure-compiler');
 const json = require('rollup-plugin-json');
 const replace = require('rollup-plugin-replace');
 const resolve = require('rollup-plugin-node-resolve');
@@ -61,7 +61,7 @@ const client = ['browse', 'main'].map(entryName => {
         limit: 5 * 1024,
         publicPath: '/_client/'
       }),
-      compiler()
+  //    compiler()
     ]
   };
 });
@@ -95,4 +95,72 @@ const server = {
   ]
 };
 
-module.exports = client.concat(server);
+module.exports = client.concat(server).concat([
+  {
+    external: ['@emotion/core', 'react', 'react-dom'],
+    input: ["browse","main"].map(entryName=>`modules/client/${entryName}.js`),
+    output: {
+      format: 'esm',
+      dir: 'public/_packages',
+      entryFileNames: '[name]-[hash].js',
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        '@emotion/core': 'emotionCore'
+      }
+    },
+    moduleContext: {
+      'node_modules/react-icons/lib/esm/iconBase.js': 'window'
+    },
+    plugins: [
+      manifest.record({ publicPath: '/_client/' }),
+      babel({ exclude: /node_modules/ }),
+      json(),
+      resolve(),
+      commonjs({
+        namedExports: {
+          'node_modules/react/index.js': [
+            'createContext',
+            'createElement',
+            'forwardRef',
+            'Component',
+            'Fragment'
+          ]
+        }
+      }),
+      replace({
+        'process.env.BUILD_ID': JSON.stringify(buildId),
+        'process.env.NODE_ENV': JSON.stringify(
+          process.env.NODE_ENV || 'development'
+        )
+      }),
+      url({
+        limit: 5 * 1024,
+        publicPath: '/_client/'
+      }),
+  //    compiler()
+    ]
+  },{
+    external: builtinModules.concat(dependencies),
+    input: ['modules/server.js'],
+    output: { file: 'public/_packages/server.js', format: 'esm' },
+    moduleContext: {
+      'node_modules/react-icons/lib/esm/iconBase.js': 'global'
+    },
+    plugins: [
+      manifest.inject({ virtualId: 'entry-manifest' }),
+      babel({ exclude: /node_modules/ }),
+      json(),
+      resolve(),
+      commonjs(),
+      url({
+        limit: 5 * 1024,
+        publicPath: '/_client/',
+        emitFiles: false
+      }),
+      replace({
+        'process.env.BUILD_ID': JSON.stringify(buildId)
+      })
+    ]
+  }
+]);
