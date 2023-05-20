c50d4f887ba18e7f47b871964a69e5be564bb56c
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-20 12:44:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import { output, channel: input } from './registry/router.js';
+import { output, channel as input } from './registry/router.js';
 output.pipeTo(new WriteableStream({write(output){ console.log(output) }}))
