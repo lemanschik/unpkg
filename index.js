9423a0f2aed04b02d9b9b911a1d5b9d375ee6f78
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-20 13:26:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import { output, channel as input } from './registry/router.js';
 output().pipeTo(new WritableStream({write(output){ console.log(output) }}))
 
 caches.open(scope).then(cache => 
-(cache.match(new Request(`${scope}/index.html`)) || 
- cache.match(new Request(`${scope}/`))) || [
-   cache.put(new Request(`${scope}/index.html`),new Response({ body: new Blob([document], { type: 'text/html' }) })),
-   cache.put(new Request(scope),new Response({ body: new Blob([document], { type: 'text/html' }) }))
+(cache.match(new Request(`registry/index.html`)) || 
+ cache.match(new Request(`registry/`))) || [
+   cache.put(new Request(`registry/index.html`),new Response({ body: new Blob([document], { type: 'text/html' }) })),
+   cache.put(new Request(`registry/`),new Response({ body: new Blob([document], { type: 'text/html' }) }))
 ]).catch(console.error);
