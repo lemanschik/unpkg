399308f1230d2203d4f573ca457188cdb9503277
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-20 11:51:43 +0200

    Update router.js

--- registry/router.js
+++ registry/router.js
@@ -1,6 +1,7 @@
 globalThis?.window && globalThis?.window.location.protocol !== 'https:' && (globalThis.window.location.protocol = 'https:');
 export const serviceWorker = globalThis.window ? globalThis.window.navigator.serviceWorker : globalThis;
-export const channel = new BroadcastChannel("service-worker::");
+const scope = new URL('./',import.meta.url);
+export const channel = new BroadcastChannel(`service-worker:${scope}`);
 
 export const concatArrayBuffer = (buffers) => Uint8Array.from(buffers.flatMap((buffer)=>[...new Uint8Array(buffer)]));
 export const GitBlobHash = (url) => fetch(url).then((r) => r.arrayBuffer()).then(
@@ -30,8 +31,9 @@ const boot = (ComponentManager={}) => Object.assign(ComponentManager,{ async boo
   ComponentManager.serviceWorker = globalThis.window.navigator.serviceWorker;
   ComponentManager.protocol = new TransformStream({transform: 
   (controller,func) => controller.enqueue(new Function(func)(ComponentManager))});
+  ComponentManager.peer = new RTCPeerConnection();
+  ComponentManager.channel = channel;
   
-  const scope = new URL('./',import.meta.url);
   await ComponentManager.serviceWorker.register(import.meta.url,
   { type: 'module', updateViaCache: 'all', scope}).then((reg) => {
     // serviceWorker.state 
@@ -75,4 +77,68 @@ new ReadableStream(boot()).pipeThrough(new TransformStream({
         c.enqueue(`System Booted ${ComponentManager.process}`)
     },
     startUI: () => {},
-})).pipeTo(new WritableStream({write(data){console.log(data)}})
+})).pipeTo(new WritableStream({write(data){console.log(data)}});
+} else {
+const serviceWorker = globalThis;
+GitHash.then(hash=>console.log("instantiation:",hash)||channel.postMessage({ "instantiating": hash }))
+  //console.log("instantiating:", await GitHash);
+
+const methods = { 
+  async skipWaiting(){
+    console.log("installing:",await GitHash);
+    channel.postMessage({ "installing:": await GitHash })
+    serviceWorker.skipWaiting();
+  }, 
+  "clients.claim": async () => {    
+    console.log("activating:",await GitHash);
+    channel.postMessage({ "activating:": await GitHash })
+    serviceWorker.clients.claim();
+    const allClients = await serviceWorker.clients.matchAll({
+      includeUncontrolled: true,
+    });
+
+    const connectedClient = allClients.find(async (cl)=>new URL(cl.url).searchParams.has(await GitHash));
+    // connectedClient?.focus();
+
+    // If we didn't find an existing chat window, // open a new one:
+    if (!connectedClient) {
+      //connectedClient = //await clients.openWindow(scope+'?'+"version="+await versionHexString);
+    }
+    channel.postMessage({ method: "window.refresh", params: [] })
+  },
+}; 
+
+serviceWorker.oninstall = (event) => event.waitUntil(methods.skipWaiting())
+serviceWorker.onactivate = (event) => event.waitUntil(methods["clients.claim"]())
+serviceWorker.onmessage = ({data: { id, method, params = [] }}) => {
+  methods[method](...params); // skipWaiting clients.claim()
+};
+
+serviceWorker.onfetch = (event) => event.waitUntil(caches.match(event.request) || caches.match(event.request,{ignoreSearch:true})||fetch(event.request));
+/** The Fundamental Concepts */
+/**
+ * You get a Boot Stream that emits a HigerOrder ComponentManager which can pass down Capabilitys.
+ * This is called Capability based Permissions you can find a lot of information on the www
+ * After Boot and Init you write to the Final Output Eg Headless, Or Logging, Or UI Or something else.
+ * On servers your write target is mostly the logging infra local or remote
+ * On Consumer UI Instances its most time the UI it self which has configuration to additional log as needed.
+ * On Development Instances you probally did forward the Streams into your IDE Some how or your IDE Even Runs with
+ * Escalated Permissions Inside the Main Component Manager for Faster iteration and debugging.
+ */
+// "compilerOptions": {
+//     "target": "esnext",
+//     "moduleResolution": "bundler",
+//     "customConditions": ["import"],
+//     "verbatimModuleSyntax": true,
+//     "composite": true,
+    
+// }
+
+// tsc --build -p ./my-project-dir 
+// --declaration
+// --emitDeclarationOnly
+// --declarationMap
+// --sourceMap
+// --inlineSourceMap
+
+}
