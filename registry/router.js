e959e42d7e40409e89cd4cc72d78516ac5dcf3fb
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-20 12:33:49 +0200

    Update router.js

--- registry/router.js
+++ registry/router.js
@@ -120,6 +120,7 @@ const document = `<!DOCTYPE html>
 <html lang="en-GB">
 <head>
 <meta charset="UTF-8">
+<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧙‍♂️</text></svg>">
 <title>Offline</title>
 <style>
 html, body, div {
