735519c7aaec0399fffbfb04baccb989c784533e
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-23 11:20:03 +0200

    Update awesome-os.module.js

--- awesome-os.module.js
+++ awesome-os.module.js
@@ -31,7 +31,20 @@ in multiple Contexts at the same time as also many other shared apis that work i
 
 audio worklets are a great example for that. 
 
+a run once worker only needs to execute code and use postMessage 
+
+
+each worker / context / world
+is a Transform Stream as they are long running tasks they accept input and produce output.
+
 */
+
+const ReadableFromPort = (port) => new ReadableStream({start(input){ port.onmessage = data => input.enqueue(data); }});
+const WritableFromPort = (port) => new WritableStream({write(output){ port.postMessage(output); }});
+const PushableStream = () => new ReadableStream({start(input){ this.push = data => input.enqueue(data); }});
+const merge = (...streams) => new ReadableStream({ async start(c){
+streams.forEach((stream) => { for await (const data of stream) { c.enqueue(data); };});
+}});
 
 // This implements a sharedSync Context that can be used with async methods 
 const location = globalThis.location || globalThis?.window.location;
@@ -42,8 +55,9 @@ const browser = (globalThis.chrome || globalThis.browser);
 const scope = new URL('./',import.meta.url);
 const name = `ÀwesomeOS`;
 
+const isNotSharedWorker = !globalThis.window && globalThis.name !== name
 
-// Runs first on initital cold Boot but after ComponentManager.serviceWorker on warm boot.
+
 // input
 export const channel = new BroadcastChannel(`components:${scope}`);
 
@@ -57,6 +71,9 @@ async (arrayBuffer) => crypto.subtle.digest("SHA-1", concatArrayBuffer([new Text
 
 // Interisting observation a Promise does not cache its last result when it gets reused its like using a function.
 let GitHash = GitBlobHash(import.meta.url).then((hash)=>(GitHash = Promise.resolve(hash)));
+
+// Runs first on initital cold Boot but after ComponentManager.serviceWorker on warm boot.
+// runs always in a sharedWorker per origin but also runs in a worker per Task if it is a higherOrderTask
 
 // use this to inject a ECMAScript Proxy for watch. 
 const getComponentManager = (watch) => 
@@ -106,15 +123,11 @@ export const SystemComponents = new TransformStream({
       // here your code runs this.startUI() for example
       // if it does not do so it will simple return the system output.
       // Audio and Video Processing also goes in here. 
-      new ReadableStream({ start(output){
-        const onmessage = ({ data }) => output.enqueue(data);
-        ComponentManager.serviceWorker.onmessage = onmessage;
-        ComponentManager.serviceWorker.startMessages(); 
-        ComponentManager.sharedWorker.onmessage = onmessage;
-        channel.onmessage = onmessage;  
-      }}).pipeThrough(ComponentManager.protocol).pipeTo(
-        new WritableStream({write(output){channel.postMessage(output); c.enqueue(output)}})
-      );
+      	
+      merge(...[ComponentManager.serviceWorker,ComponentManager.sharedWorker, channel].map(ReadableFromPort)).pipeThrough(
+      ComponentManager.protocol).pipeTo(new WritableStream({write(output){channel.postMessage(output); c.enqueue(output)}}));
+      ComponentManager.serviceWorker.startMessages(); 
+
       // MOTD
       c.enqueue({ stdout: `System Booted ${ComponentManager.process.name}` })
     },
@@ -123,38 +136,88 @@ export const SystemComponents = new TransformStream({
 
 export const awesomeBuildInModules = [SystemComponents];
 
-export const output = (!globalThis.window) 
-? new ReadableStream({start(output){ channel.onmessage = msg => output.enqueue(msg); }}) 
+const isNotWindow = !globalThis.window;
+const isAwesomeOS = globalThis.name === name;
+
+export const output = isNotWindow
+? ReadableFromPort(channel)
 : awesomeBuildInModules.reduce((stream,module,_id) =>
 (stream = stream.pipeThrough(module)),
 getComponentManager());
-  //.pipeTo(new WritableStream({write(data){console.log(data)}});
+//.pipeTo(new WritableStream({write(data){console.log(data)}});
 
-if (!globalThis.window && globalThis.name === name) {
+const isSharedWorker = isNotWindow && isAwesomeOS;
+const isServiceWorker = isNotWindow && !isAwesomeOS && globalThis.self;
+
+if (isSharedWorker) {
   // used to send signals to the serviceWorker for background services.
   const serviceWorker = new BroadcastChannel(`service-worker:${scope}`);
   // Handels messaging between contexts and components. Is the globalSource of turth.
   // for the given origin and scope inside of an instance or browser session.
   const sharedWorker = globalThis;
   const protocol = new TransformStream({ async transform([functionBody,port],controller) {
-	try {
-	  const stdout = await new Function(`return ${functionBody}`)();
-	  port.postMessage(stdout);
-	  controller.enqueue({ stdout });
-	});
-	} catch (err) {
-	const errorOutput = { stderr: `${err} ${err.message||''} ${err.stack||''}` }
-	port.postMessage(errorOutput)
-	c.enqueue(errorOutput)  
-	}
-	}});
+  try {
+  const stdout = await new Function(`return ${functionBody}`)();
+  port.postMessage(stdout);
+  controller.enqueue({ stdout });
+  });
+  } catch (err) {
+  const errorOutput = { stderr: `${err} ${err.message||''} ${err.stack||''}` }
+  port.postMessage(errorOutput)
+  c.enqueue(errorOutput)  
+  }
+  }});
    
-   sharedWorker.onconnect = ({ports:[port]}) => new ReadableStream({start(tasks){ 
-	   port.onmessage = (task) => tasks.enqueue([task,port]);
-   }}).pipeThrough(protocol)
+  sharedWorker.onconnect = ({ports:[port]}) => ReadableFromPort(port).pipeThrough(protocol);
   
+} else if (isServiceWorker) {
+ 
+// BootScreen Graphical
+const document = `<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+<meta charset="UTF-8">
+<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧙‍♂️</text></svg>">
+<title>Offline</title>
+<style>
+html, body, div {
+width:  100%;
+height:  100%;
+margin:  0;
+padding:  0;
+}
+body {
+font-family: sans-serif;
+color: white;
+font-size: 100px;
+}
+body * {
+position: relative;
+}
+#one {
+background: rgb(2,0,36);
+background: radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(25,25,205,1) 50%, rgba(0,212,255,1) 100%);
+display: flex;
+justify-content: center;
+align-items: center;
+}
+</style>
+</head>
+<body>
+<div id="one">
+	<span>Offline</span>
+</div>
 
-} else if (!globalThis.window && globalThis.name !== name) {
+</body></html>`;
+
+caches.open(scope).then(async cache => 
+(await cache.match(new Request(`${scope}/index.html`)) || 
+await cache.match(new Request(`${scope}/`))) || [
+await cache.put(new Request(`${scope}/index.html`),new Response({ body: new Blob([document], { type: 'text/html' }) })),
+await cache.put(new Request(scope),new Response({ body: new Blob([document], { type: 'text/html' }) }))
+]);
+
+	
 GitHash.then(hash=>console.log("instantiation:",hash)||channel.postMessage({ "instantiating": hash }))
   //console.log("instantiating:", await GitHash);
 
@@ -187,63 +250,18 @@ serviceWorker.oninstall = (event) => event.waitUntil(methods.skipWaiting())
 serviceWorker.onactivate = (event) => event.waitUntil(methods["clients.claim"]())
 
 serviceWorker.onmessage = ({data: { id, method, params = [] }}) => {
-  // Note: we could also use channel it would produce the same result
-  serviceWorker.postMessage({ id, data: `(ComponentManager) => ({ id: ${id}, data: ${methods[method](...params)}, });` }); 
-  // skipWaiting clients.claim()
+// Note: we could also use channel it would produce the same result
+serviceWorker.postMessage({ id, data: `(ComponentManager) => ({ id: ${id}, data: ${methods[method](...params)}, });` }); 
+// skipWaiting clients.claim()
 };
-  
-// BootScreen Graphical
-const document = `<!DOCTYPE html>
-<html lang="en-GB">
-<head>
-<meta charset="UTF-8">
-<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧙‍♂️</text></svg>">
-<title>Offline</title>
-<style>
-html, body, div {
-	width:  100%;
-	height:  100%;
-	margin:  0;
-	padding:  0;
-}
-body {
-	    font-family: sans-serif;
-	    color: white;
-			font-size: 100px;
-	}
-body * {
-			position: relative;
-		}
-#one {
-			background: rgb(2,0,36);
-			background: radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(25,25,205,1) 50%, rgba(0,212,255,1) 100%);
-			display: flex;
-			justify-content: center;
-			align-items: center;
-		}
-	</style>
-</head>
-<body>
-	<div id="one">
-		<span>Offline</span>
-	</div>
-
-</body></html>`;
-
-caches.open(scope).then(async cache => 
-(await cache.match(new Request(`${scope}/index.html`)) || 
- await cache.match(new Request(`${scope}/`))) || [
-   await cache.put(new Request(`${scope}/index.html`),new Response({ body: new Blob([document], { type: 'text/html' }) })),
-   await cache.put(new Request(scope),new Response({ body: new Blob([document], { type: 'text/html' }) }))
-]);
 
 serviceWorker.onfetch = async (event) => {
-	serviceWorker.postMessage({ data: 'fetch', event.request.url) });
-	return await caches.match(event.request.url) || 
-		await caches.match(event.request.url,{ignoreSearch:true}) || fetch(event.request.url).then(
-  			r => `${r.status}`.startsWith('2') ? r : new Response({ 
-			status: 404, body: new Blob([`Request: ${event.request.url} Not Found.`], { type: 'text/html' 
-		}) }) );
+serviceWorker.postMessage({ data: 'fetch', event.request.url) });
+return await caches.match(event.request.url) || 
+await caches.match(event.request.url,{ignoreSearch:true}) || fetch(event.request.url).then(
+r => `${r.status}`.startsWith('2') ? r : new Response({ 
+status: 404, body: new Blob([`Request: ${event.request.url} Not Found.`], { type: 'text/html' 
+}) }) );
 }
 /** The Fundamental Concepts */
 /**
